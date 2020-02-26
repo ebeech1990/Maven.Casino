e404db56b81a35a12e108515ee73221adb689d78
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-26 15:33:21 -0500

    added gofishhuman

--- src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
+++ src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
@@ -1,4 +1,27 @@
 package io.zipcoder.casino.gamePlayers;
 
-public class GoFishHumanPlayer {
-}
+import io.zipcoder.casino.CardPlayer;
+import io.zipcoder.casino.Wallet;
+
+public class GoFishHumanPlayer extends CardPlayer
+{
+    public Integer score = 0;
+
+    public GoFishHumanPlayer(String name, Wallet gamblingWallet) {
+        super(name, gamblingWallet);
+    }
+
+
+    public Boolean fishing(Integer number)
+    {
+
+        return false;
+    }
+
+    public Boolean takeSet(Integer number)
+    {
+        //if player has set increase score by 1
+        //remove sets from player hand?
+        return false;
+    }
+}git
