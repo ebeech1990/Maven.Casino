f3f42adbe44014030945639c30b43bd367c68a07
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-26 15:53:41 -0500

    initial skeleton for dealer

--- src/main/java/io/zipcoder/casino/gamePlayers/BlackJackNPC.java
+++ src/main/java/io/zipcoder/casino/gamePlayers/BlackJackNPC.java
@@ -1,4 +1,19 @@
 package io.zipcoder.casino.gamePlayers;
 
-public class BlackJackNPC {
+import io.zipcoder.casino.CardPlayer;
+import io.zipcoder.casino.Wallet;
+
+public class BlackJackNPC extends CardPlayer
+{
+    public BlackJackNPC(String name, Wallet gamblingWallet) {
+        super(name, gamblingWallet);
+    }
+
+    public void underSeventeen(Integer handValue){
+        if(handValue < 17){
+            this.hand.add(Deck[0]);
+        }
+    }
+
+
 }
