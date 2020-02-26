a3acbeca6b18552e535ac46a6ab4ea89d16ca7ce
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-26 17:13:06 -0500

    Need to work on Blackjack player

--- src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
+++ src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
@@ -1,22 +1,48 @@
 package io.zipcoder.casino.gamePlayers;
 
+import io.zipcoder.casino.Card;
 import io.zipcoder.casino.CardPlayer;
+import io.zipcoder.casino.Wallet;
 
-//public class GoFishHumanPlayer extends CardPlayer {
-//    public Integer score = 0;
-//
-//    public GoFishHumanPlayer(){
-//        super();
-//
-//    }
-//
-//    public Boolean fishing(Integer number){
-//        return false;
-//    }
-//
-//    public Boolean takeSet(Integer number){
-//        //if player has set increase score by 1
-//        //remove sets from player hand?
-//        return false;
-//    }
-//}
+import java.util.ArrayList;
+
+public class GoFishHumanPlayer extends CardPlayer {
+    private Integer score = 0;
+
+    public GoFishHumanPlayer(String name, Wallet gamblingWallet){
+        super(name, gamblingWallet);
+
+    }
+
+    public Integer getScore()
+    {
+        return score;
+    }
+
+    public Boolean fishing(CardPlayer target, Card.Rank desiredRank)
+    {
+
+
+        return false;
+    }
+
+    public ArrayList<Card> fishedFrom(Integer number)
+    {
+        ArrayList<Card> fishedCards = new ArrayList<>();
+
+        for(Card card : this.hand)
+        {
+
+        }
+
+        return fishedCards;
+    }
+
+    public Boolean takeSet(Card.Rank set)
+    {
+
+        //if player has set increase score by 1
+        //remove sets from player hand?
+        return false;
+    }
+}
