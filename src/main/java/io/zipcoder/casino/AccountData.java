edf10e157ce59ec94adc06501a844ada9cbace00
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-26 10:08:55 -0500

    account data and test stubs

--- src/main/java/io/zipcoder/casino/AccountData.java
+++ src/main/java/io/zipcoder/casino/AccountData.java
@@ -1,4 +1,23 @@
 package io.zipcoder.casino;
 
 public class AccountData {
+
+    private Wallet userWallet;
+    private Integer id;
+
+    public void setWallet(Wallet w){
+
+    }
+
+    public Wallet getWallet() {
+        return userWallet;
+    }
+
+    public void setId(Integer id) {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
 }
