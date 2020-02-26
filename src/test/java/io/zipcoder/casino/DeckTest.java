4bbbea9ed8ad40b4732a84c1c82733607f5c5745
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-26 13:46:00 -0500

    Wallet/feature merge issues resolved

--- src/test/java/io/zipcoder/casino/DeckTest.java
+++ src/test/java/io/zipcoder/casino/DeckTest.java
@@ -1,23 +1,16 @@
 package io.zipcoder.casino;
 
-<<<<<<< HEAD
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DeckTest {
     @Test
 
-    public void getACard()
-    {
+    public void getACard() {
         Deck testDeck = new Deck();
 
         Object actual = testDeck.deal();
 
         Assert.assertTrue(actual instanceof Card);
     }
-=======
-public class DeckTest {
-
-
->>>>>>> ebe4fe5f7f22aae317bfcb5180970a041e78b438
 }
