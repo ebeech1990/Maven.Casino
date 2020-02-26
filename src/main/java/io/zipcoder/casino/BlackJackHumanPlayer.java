206b9bd732044b998aebabbdc8eed230a2f32be2
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-26 15:19:33 -0500

    added blackjack skeleton

---
+++ src/main/java/io/zipcoder/casino/BlackJackHumanPlayer.java
@@ -0,0 +1,21 @@
+package io.zipcoder.casino;
+
+public class BlackJackHumanPlayer extends CardPlayer {
+
+
+    public BlackJackHumanPlayer(String name, Wallet gamblingWallet) {
+        super(name, gamblingWallet);
+    }
+
+    public Boolean doubleDown(){
+        // if true double wager
+        return null;
+    }
+
+    public Boolean split(){
+        //player has option to split only if both cards match
+
+        return null;
+    }
+
+}
