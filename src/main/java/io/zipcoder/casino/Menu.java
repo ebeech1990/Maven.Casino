79ef878d0dcac2538a919b8facd1b58079d61dc7
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-26 10:04:13 -0500

    menu and test stubs

--- src/main/java/io/zipcoder/casino/Menu.java
+++ src/main/java/io/zipcoder/casino/Menu.java
@@ -1,4 +1,20 @@
 package io.zipcoder.casino;
 
 public class Menu {
+
+    public void chooseGame(Integer game) {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void quitApp() {
+
+    }
+
+    public AccountData createAccount() {
+        return null;
+    }
 }
