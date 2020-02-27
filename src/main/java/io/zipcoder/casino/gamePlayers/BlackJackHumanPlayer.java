2478f720158262e3d684dc4aa24e71d68dfb2b5d
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-27 15:57:51 -0500

    added split method

--- src/main/java/io/zipcoder/casino/gamePlayers/BlackJackHumanPlayer.java
+++ src/main/java/io/zipcoder/casino/gamePlayers/BlackJackHumanPlayer.java
@@ -1,10 +1,16 @@
 package io.zipcoder.casino.gamePlayers;
 
+import io.zipcoder.casino.Card;
 import io.zipcoder.casino.CardPlayer;
+import io.zipcoder.casino.Deck;
 import io.zipcoder.casino.Wallet;
 
-public class BlackJackHumanPlayer extends CardPlayer {
+import java.util.ArrayList;
 
+public class BlackJackHumanPlayer extends CardPlayer
+
+{
+    private ArrayList<Card> secondHand;
 
     public BlackJackHumanPlayer(String name, Wallet gamblingWallet) {
         super(name, gamblingWallet);
@@ -12,14 +18,24 @@ public class BlackJackHumanPlayer extends CardPlayer {
 
     public Boolean doubleDown(){
         // TODO: if true double wager
-
-        return null;
+        if(true){
+            // double Wager / bet initial wager again and receive 1 final card
+            this.receiveCard();
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public Boolean split(){
-        // TODO: player has option to split only if both cards match
-
-        return null;
+        Card card1 = this.getHand().get(0);
+        Card card2 = this.getHand().get(1);
+        if(card1.compareTo(card2) == 0){
+            secondHand.add(this.getHand().remove(0));
+            return true;
+        }
+        return false;
     }
 
 }
