4a2cf3ec6729a8f44ee25bd8268340fb0001a3b6
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-02-26 20:46:43 -0500

    updating classes

--- src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
+++ src/main/java/io/zipcoder/casino/gamePlayers/GoFishHumanPlayer.java
@@ -2,21 +2,18 @@ package io.zipcoder.casino.gamePlayers;
 
 import io.zipcoder.casino.CardPlayer;
 
-public class GoFishHumanPlayer extends CardPlayer {
-    public Integer score = 0;
-
-    public GoFishHumanPlayer(){
-        super();
-
-    }
-
-    public Boolean fishing(Integer number){
-        return false;
-    }
-
-    public Boolean takeSet(Integer number){
-        //if player has set increase score by 1
-        //remove sets from player hand?
-        return false;
-    }
-}
+//public class GoFishHumanPlayer extends CardPlayer {
+//    public Integer score = 0;
+//
+//
+//
+//    public Boolean fishing(Integer number){
+//        return false;
+//    }
+//
+//    public Boolean takeSet(Integer number){
+//        //if player has set increase score by 1
+//        //remove sets from player hand?
+//        return false;
+//    }
+//}
