18a68ca7549ac3176ec8e8cce96eb438172f8507
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-03-01 13:18:50 -0500

    Hand and BJHand working

--- src/main/java/io/zipcoder/casino/Hand.java
+++ src/main/java/io/zipcoder/casino/Hand.java
@@ -1,34 +1,20 @@
 package io.zipcoder.casino;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Hand {
     ArrayList<Card> currentHand;
-    HashMap<Card.Suit, Integer> handSuitInfo;
 
     public Hand()
     {
-        currentHand = new ArrayList<>();
-        handSuitInfo = new HashMap<>();
+        this(new ArrayList<Card>());
     }
 
     public Hand(List<Card> cardsForHand)
     {
         currentHand = new ArrayList<>();
-        handSuitInfo = new HashMap<>();
         currentHand.addAll(cardsForHand);
-    }
-
-    public Hand(Card ... startingCards)
-    {
-        currentHand = new ArrayList<>();
-        handSuitInfo = new HashMap<>();
-        for(Card card : startingCards)
-        {
-            currentHand.add(card);
-        }
     }
 
     public ArrayList<Card> getCurrentHand()
@@ -36,52 +22,15 @@ public class Hand {
         return currentHand;
     }
 
-    public HashMap<Card.Suit, Integer> getHandSuitInfo()
-    {
-        return handSuitInfo;
-    }
-
     public Boolean addCard(Card cardToBeAdded)
     {
-        Card.Suit addedCardSuit = cardToBeAdded.suit();
         currentHand.add(cardToBeAdded);
-
-        if(handSuitInfo.containsKey(addedCardSuit))
-        {
-            Integer currentSuitCount = handSuitInfo.get(addedCardSuit);
-            currentSuitCount++;
-            handSuitInfo.put(addedCardSuit, currentSuitCount);
-        }
-        else
-        {
-            handSuitInfo.put(addedCardSuit, 1);
-        }
-
         return currentHand.contains(cardToBeAdded);
     }
 
     public Boolean removeCard(Card cardToRemove)
     {
-        Card.Suit removedCardSuit = cardToRemove.suit();
         currentHand.remove(cardToRemove);
-
-        if(handSuitInfo.containsKey(cardToRemove))
-        {
-            Integer currentSuitCount = handSuitInfo.get(cardToRemove);
-            currentSuitCount--;
-            if(currentSuitCount > 1)
-            {
-                handSuitInfo.remove(removedCardSuit);
-            }
-            else
-            {
-                handSuitInfo.remove(removedCardSuit);
-            }
-        }
-        else
-        {
-            handSuitInfo.remove(removedCardSuit);
-        }
 
         return (!currentHand.contains(cardToRemove));
     }
--- src/test/java/io/zipcoder/casino/HandTest.java
+++ src/test/java/io/zipcoder/casino/HandTest.java
@@ -1,5 +1,7 @@
 package io.zipcoder.casino;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,9 +9,23 @@ import java.util.List;
 
 import static io.zipcoder.casino.Card.Rank.*;
 import static io.zipcoder.casino.Card.Suit.*;
+import static org.junit.Assert.assertEquals;
 
 public class HandTest {
+    Hand myHand;
+    Hand handOfTwos;
+    ArrayList<Card> allDeuces;
 
+    @Before
+    public void setup()
+    {
+        allDeuces = new ArrayList<>();
+        allDeuces.add(new Card(DEUCE, SPADES));
+        allDeuces.add(new Card(DEUCE, CLUBS));
+        allDeuces.add(new Card(DEUCE, DIAMONDS));
+        allDeuces.add(new Card(DEUCE, HEARTS));
+        Hand handOfTwos = new Hand(allDeuces);
+    }
 
     @Test
     public void constructorTest()
@@ -19,9 +35,5 @@ public class HandTest {
         listOfCards.add(new Card(ACE, SPADES));
         Hand testHand = new Hand();
         Hand testHandFromList = new Hand(listOfCards);
-        Hand testHandWithNewCards = new Hand(new Card(JACK, DIAMONDS));
     }
-
-//    @Test
-//    public void
 }
